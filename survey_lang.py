70761ed5f002e89c395daf04cf401b5fe7d0c6ba
Yaroslav Kobel <yarokobel@gmail.com>
2020-07-28 10:11:51 +0300

    creare file and coding survey_lang

---
+++ survey_lang.py
@@ -0,0 +1,16 @@
+from survey import AnonymousSurvey
+
+question = "Какой язык программирования вас найболее интересен?"
+
+my_survey = AnonymousSurvey(question)
+
+my_survey.show_question()
+print("Нажмите 'q' для выхода с опроса. \n")
+while True:
+    response = input("Language: ")
+    if response == 'q':
+        break
+    my_survey.store_response(response)
+
+print("\n Спасибо за ответ")
+my_survey.show_results()
