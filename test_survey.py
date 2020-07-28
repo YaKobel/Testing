9e112c823c241db8d922e04a166dfeb7846682cd
Yaroslav Kobel <yarokobel@gmail.com>
2020-07-28 11:03:00 +0300

    add five responses

--- test_survey.py
+++ test_survey.py
@@ -2,7 +2,7 @@ import unittest
 from survey import AnonymousSurvey
 
 class TestAnonmySurvey(unittest.TestCase):
-    """Тесты для класов"""
+    """Тесты для класов AnonymousSurvey"""
 
     def test_store_single_response(self):
         """Проверяется, что один ответ сохранен в списке"""
@@ -11,6 +11,18 @@ class TestAnonmySurvey(unittest.TestCase):
         my_survey.store_response('Java')
 
         self.assertIn('Java', my_survey.responses)
+
+    def test_five_responses(self):
+        """Проверяет что 5 ответов были сохраненны"""
+
+        question = "Какой язык программирования вам по душе?"
+        my_survey = AnonymousSurvey(question)
+        responses5 = ['Java', 'Python', 'C#', 'Go', 'JavaScript']
+        for response in responses5:
+            my_survey.store_response(response)
+
+        for response in responses5:
+            self.assertIn(response, my_survey.responses)
 
 if __name__ == '__name__':
     unittest.main()
