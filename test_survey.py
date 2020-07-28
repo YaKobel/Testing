d86cb86ce035ab98037694708fe21482e72f64ce
Yaroslav Kobel <yarokobel@gmail.com>
2020-07-28 10:40:53 +0300

    create class test survey

--- test_survey.py
+++ test_survey.py
@@ -1,1 +1,16 @@
 import unittest
+from survey import AnonymousSurvey
+
+class TestAnonmySurvey(unittest.TestCase):
+    """Тесты для класов"""
+
+    def test_store_single_response(self):
+        """Проверяется, что один ответ сохранен в списке"""
+        question = "Какой язык программирования вас найболее интересен?"
+        my_survey = AnonymousSurvey(question)
+        my_survey.store_response('Java')
+
+        self.assertIn('Java', my_survey.responses)
+
+if __name__ == '__name__':
+    unittest.main()
